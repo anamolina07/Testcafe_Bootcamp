9f8be7bfef89eff9ce038ce2743e7961c6770dec
Ana Molina <ana.molina@nuxiba.com>
2020-10-22 16:24:26 -0500

    Cambios en los archivos referentes a las practicas de clase

--- Testcafe/pageModel.js
+++ Testcafe/pageModel.js
@@ -6,7 +6,7 @@ class Page {
     //this.link2 = Selector('#content > ul > li:nth-child(1) > a');
     this.link2 = Selector('a').withText('A/B Testing');  
     //this.text21 = Selector('h3').withText('A/B Test Variation 1');
-    this.text21 = Selector('#content > div > h3');
+    this.text21 = Selector('#content > div > h3');     
 
     // Practica 2
     this.link3 = Selector('a').withText('Forgot Password');
@@ -17,9 +17,9 @@ class Page {
     // Practica 4
     this.link4 = Selector('a').withText('Checkboxes');
     //Checkbox padre
-    this.baseCheckbox = Select('#checkboxes');
- 
-    //Checkbox hijo
+    this.baseCheckbox = Selector('#checkboxes > input[type=checkbox]');   
+
+    //Checkbox hijo --> en caso de que se quiera ser muy especifico en el nombre del componente que se está localizando
     //this.checkbox41 = Selector('#checkboxes > input[type=checkbox]:nth-child(1)');
     //this.checkbox42 = Selector('#checkboxes > input[type=checkbox]:nth-child(3)');
     }

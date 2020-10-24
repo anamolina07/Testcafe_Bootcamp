d85f2703f1166faa2870e96cf03b5ee17f3a847e
Ana Molina <ana.molina@nuxiba.com>
2020-10-23 22:26:11 -0500

    Formato al código de practica2.js

--- Testcafe/practica3.js
+++ Testcafe/practica3.js
@@ -23,14 +23,14 @@ test('Probar que podemos escribir un email', async t =>{
 
 test('Probar envio correcto de email', async t =>{
     await t
-    .click(page.link3)
+        .click(page.link3)
 
     await t
-    .typeText(page.input31, 'noemail@noemail.com')
-    .expect(page.input31.value).eql('noemail@noemail.com')
-    .click(page.button32)
+        .typeText(page.input31, 'noemail@noemail.com')
+        .expect(page.input31.value).eql('noemail@noemail.com')
+        .click(page.button32)
 
     await t
-    //.expect(page.text21.innerText).contains('A/B Test Variation 1');
-    .expect(page.text33.innerText).contains('sent','Prueba exitosa')
+    //  .expect(page.text21.innerText).contains('A/B Test Variation 1');
+        .expect(page.text33.innerText).contains('sent','Prueba exitosa')
 })
